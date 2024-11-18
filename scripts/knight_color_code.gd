5561ba1b8daf9c71939a9f5cc366988fd1609c63
Isaac RF <IsaacRF239@gmail.com>
2024-11-18 20:05:38 +0100

    Add code shader and multiple color customization

--- scripts/knight_color_code.gd
+++ scripts/knight_color_code.gd
@@ -1,4 +1,8 @@
 extends AnimatedSprite2D
 
-func _on_color_picker_color_changed(color):
-	material.set_shader_parameter("replacement_color", color)
+func _on_feather_color_picker_color_changed(color):
+	material.set_shader_parameter("feather_color", color)
+
+
+func _on_cape_color_picker_color_changed(color):
+	material.set_shader_parameter("cape_color", color)
